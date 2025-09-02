1449c1e2a9165337ee4b4eee9329ad801b4f748b
Zulfikar Azmi <zazmialghifari@gmail.com>
2025-09-02 19:10:25 +0700

    feat(add cors)

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"time"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 
 	"backend-kking/backend/config"
 	"backend-kking/backend/routes"
@@ -10,6 +12,20 @@ import (
 func main() {
 	r := gin.Default()
 	config.ConnectDB()
+
+	// CORS setup
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	// Routes
 	routes.SetupRoutes(r)
+
+	// Run server
 	r.Run(":8080")
 }
